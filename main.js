535e970d61d57b61dcd4e1e6f12ac852f2967219
Paulo <GitPaulo@users.noreply.github.com>
2020-02-08 23:22:33 +0000

    Commented code

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
-'use strict';
-
-// goodbye this app needs no security lol
+// Disable warnings
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
-const {
-    app,
-    BrowserWindow
-} = require('electron');
+// Imports
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
 
+// Constants
 const INDEX_FOLDER_PATH = path.join(__dirname, 'views', 'home');
-// Most common resolution :)
 const WINDOW_WIDTH  = 1366;
 const WINDOW_HEIGHT = 768;
 
+// Outer scope main window
 let mainWindow;
 
+// Wrap around function
 function createWindow() {
+    // Instantiate BrowserWindow instance
     mainWindow = new BrowserWindow({
         width: WINDOW_WIDTH,
         height: WINDOW_HEIGHT,
         center: true,
         alwaysOnTop: false,
-        useContentSize: true // when false, width/height will set the size of the whole app, including frames. If true, innerWindow will be set instead, resulting in a bigger app window
+        useContentSize: true
     });
 
     // Load the index.html of the app
@@ -33,7 +32,7 @@ function createWindow() {
         slashes: true
     }));
 
-    //mainWindow.webContents.openDevTools("undock")
+    // mainWindow.webContents.openDevTools("undock")
 
     // Emitted when the window is closed
     mainWindow.on('closed', function () {
@@ -44,13 +43,14 @@ function createWindow() {
 // This method will be called when Electron has finished
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
+// Quit when all windows are closed
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
+// Activation event
 app.on('activate', function () {
     if (mainWindow === null) {
         createWindow();
